816ff7d557b056dc8fff1fa674cc647b31ddf408
Konstantin Semyanystyi <konstyantyn.semyanystyi@gmail.com>
2020-03-23 13:45:15 +0200

    '.'

--- books/delete.js
+++ books/delete.js
@@ -10,31 +10,14 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 Object.defineProperty(exports, "__esModule", { value: true });
 const uuid = require("uuid");
 const dyndb_1 = require("../util/dyndb");
-function validate(data) {
-    const defaultMsg = 'Couldn\'t create the book item.';
-    if (typeof data.name !== 'string') {
-        console.error('Name Validation Failed');
-        throw new Error('Invalid Name.');
-    }
-    if (Number.isNaN(Date.parse(data.releaseDate))) {
-        console.error('Validation Failed');
-        throw new Error('Couldn\'t create the book item.');
-    }
-    if (typeof data.authorName !== 'string') {
-        console.error('Validation Failed');
-        throw new Error('Couldn\'t create the book item.');
-    }
-    return data;
-}
 module.exports.create = (event, context) => __awaiter(this, void 0, void 0, function* () {
-    // const data = JSON.parse(event.body)
-    const data = validate(event.body);
+    const data = JSON.parse(event.body);
     const params = {
         TableName: process.env.DYNAMODB_TABLE,
         Item: {
             uuid: uuid.v1(),
             name: data.name,
-            releaseDate: Date.parse(data.releaseDate),
+            releaseDate: data.releaseDate,
             authorName: data.authorName
         }
     };
