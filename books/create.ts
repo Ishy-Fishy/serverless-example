f8fd97acac566546675ec9ce7d7c564992d48c38
Konstantin Semyanystyi <konstyantyn.semyanystyi@gmail.com>
2020-03-23 14:31:22 +0200

    ','

--- books/create.ts
+++ books/create.ts
@@ -8,40 +8,41 @@ function validate(data) {
     const defaultMsg = 'Couldn\'t create the book item.';
     if (typeof data.name !== 'string') {
         console.error('Name Validation Failed')
-        throw new Error('Invalid Name.')
+        throw new Error('Invalid book name.')
     }
     if (Number.isNaN(Date.parse(data.releaseDate))) {
         console.error('Validation Failed')
-        throw new Error('Couldn\'t create the book item.')
+        throw new Error('Invalid Date')
     }
     if (typeof data.authorName !== 'string') {
         console.error('Validation Failed')
-        throw new Error('Couldn\'t create the book item.')
+        throw new Error('Invalid author name')
     }
     return data;
 }
 
 module.exports.create = async (event, context) => {
+    throw new Error(`Body: ${JSON.stringify(event.body)}`);
     // const data = JSON.parse(event.body)
-    console.log(event);
-    const data = validate(event.body);
+    // console.log(event);
+    // const data = validate(event.body);
 
-    const params = {
-        TableName: process.env.DYNAMODB_TABLE,
-        Item: {
-            uuid: uuid.v1(),
-            name: data.name,
-            releaseDate: Date.parse(data.releaseDate),
-            authorName: data.authorName
-        }
-    }
-
-    const result = await putAsync(params);
-    // create a response
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify(result.Item)
-    }
-    return response
-
+    // const params = {
+    //     TableName: process.env.DYNAMODB_TABLE,
+    //     Item: {
+    //         uuid: uuid.v1(),
+    //         name: data.name,
+    //         releaseDate: Date.parse(data.releaseDate),
+    //         authorName: data.authorName
+    //     }
+    // }
+    //
+    // const result = await putAsync(params);
+    // // create a response
+    // const response = {
+    //     statusCode: 200,
+    //     body: JSON.stringify(result.Item)
+    // }
+    // return response
+    return "";
 }
